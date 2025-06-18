d82e2f700a75abaf528b5be94ff35ddbc5da482e
Mikołaj P. <107580930+Miks007@users.noreply.github.com>
2025-06-18 11:29:01 +0200

    Update Check_view_columns_and_data_types.sql

--- Redshift/Check_view_columns_and_data_types.sql
+++ Redshift/Check_view_columns_and_data_types.sql
@@ -1,6 +1,6 @@
 select pg_get_cols('schema.view_name');
 
 
-select * from pg_get_cols('us_newton_local.v_t_pr_msm_cm_data_processinteract')
+select * from pg_get_cols('schema.view_name')
 cols(view_schema name, view_name name, col_name name, col_type varchar, col_num int)
 where col_type like '%timestamp%';
